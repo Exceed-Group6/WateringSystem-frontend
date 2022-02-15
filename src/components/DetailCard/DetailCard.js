5a7b27aa228f68837afbcfff8a9fac077a5114c1
wave49192 <sirawich.tu@ku.th>
2022-02-15 20:20:11 +0700

    add node_module

---
+++ src/components/DetailCard/DetailCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './DetailCard.css'
+
+const DetailCard = () => {
+    return (
+        <div className="container">
+            <div className="container-left">Test</div>
+            <div className="container-right">Test</div>
+        </div>
+    )
+}
+
+export default DetailCard
