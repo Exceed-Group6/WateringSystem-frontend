168bc6bb81ecdef4e92e2d4252e5f16b6135a829
wave49192 <sirawich.tu@ku.th>
2022-02-16 13:07:58 +0700

    add config and watering button

--- src/components/DetailCard/DetailCard.js
+++ src/components/DetailCard/DetailCard.js
@@ -10,7 +10,7 @@ const DetailCard = () => {
 
                     <img src="./tree.jpeg" alt="tree"></img>
                     <div className="description">
-                        Description
+                        <h3 color="gray">Description</h3>
                         <div>lorem ipsum dolor sit amet, consectetur adipiscing el</div>
 
                     </div>
@@ -46,6 +46,44 @@ const DetailCard = () => {
                                 </div>
                             </div>
                         </div>
+
+                    </div>
+                    <div className="container-right">
+
+
+                        <div className="container-right-img2">
+
+                            <img src="./hourglass.png" alt="water"></img>
+                        </div>
+                        <div className="time-button">10 Sec</div>
+                        <button className="watering-button">
+                            <img src="./watering-plant.png" alt="watercan"></img>
+                        </button>
+                        <div className="container-right-progress">
+
+
+
+                        </div>
+
+                    </div>
+                    <div className="container-right">
+
+                        <a href="#" className="preference-button">Preference configuration
+
+                        </a>
+                        <a href="#">
+                            <img className="pref-img" src="./edit.png" alt="edit" ></img>
+                        </a>
+
+                    </div>
+                    <div className="container-right">
+                        <a href="#" className="a-graph">
+                            <p className="graph-button">
+                                <img src="./graph.png" alt="graph"></img>
+                            </p>
+
+
+                        </a>
 
                     </div>
                 </div>
