2cbb95df60f5965414379ee3efa651c5a7e4fd0d
wave49192 <sirawich.tu@ku.th>
2022-02-16 11:44:08 +0700

    Add mock up progress  bar

--- src/components/DetailCard/DetailCard.js
+++ src/components/DetailCard/DetailCard.js
@@ -15,17 +15,37 @@ const DetailCard = () => {
 
                     </div>
                 </div>
+                <div class="d-flex justify-content-around">
+                    <div className="container-right">
+                        <div className="containter-right-text">
+                            <h2>1800 Lux</h2>
+                            <h2>54%</h2>
+                            <h2>34°c</h2>
+                        </div>
 
-                <div className="container-right">
-                    <div className="containter-right-text">
-                        <h2>1800 Lux</h2>
-                        <h2>54%</h2>
-                        <h2>34°c</h2>
-                    </div>
-                    <div className="container-right-img">
-                        <img src="./sun.jpeg" alt="sun"></img>
-                        <img src="./water.jpeg" alt="water"></img>
-                        <img src="./temp.jpeg" alt="temp"></img>
+                        <div className="container-right-img">
+                            <img src="./sun.jpeg" alt="sun"></img>
+                            <img src="./water.jpeg" alt="water"></img>
+                            <img src="./temp.jpeg" alt="temp"></img>
+                        </div>
+                        <div className="container-right-progress">
+
+
+                            <div class="w3-container">
+                                <div class="w3-light-grey">
+                                    <div class="w3-green"></div>
+                                </div><br></br>
+
+
+                                <div class="w3-light-grey">
+                                    <div class="w3-blue"></div>
+                                </div><br></br>
+
+                                <div class="w3-light-grey">
+                                    <div class="w3-red"></div>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
