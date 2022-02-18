47709ba5fab368494d950fbc9981b7f165769a4b
wave49192 <sirawich.tu@ku.th>
2022-02-18 20:53:20 +0700

    add api on home page

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,15 +1,17 @@
 import React from "react";
 import DetailCard from "../../components/DetailCard/DetailCard";
 import "./Detail.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
 const Detail = () => {
+  // const { treeId } = useParams();
   return (
     <body className='Detail'>
       <nav className='sticky-top navbar navbar-expand-md navbar-light bg-success'>
         <Link to={`/Home`}>
           <i class='fa-solid fa-angles-left fa-2xl'></i>
         </Link>
-        <h1 className='title'>Tree 1 </h1>
+        <h1 className='title'>Tree </h1>
       </nav>
       <div>
         {" "}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/card/Card";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { getAllTree } from "../services/api";
 export const Home = () => {
+  const [treeList, setTreeList] = useState([]);
+
+  useEffect(() => {
+    const count = setInterval(() => {
+      getAllTree().then((data) => {
+        setTreeList(data.result);
+      });
+      return () => {
+        clearInterval(count);
+      };
+    }, 1000);
+  }, []);
+  console.log(treeList.result);
+
   return (
     <div className='Home'>
       <nav className='sticky-top navbar navbar-expand-md navbar-light bg-success'>
         <h1 className='Home-title'>Auto Watering System </h1>
       </nav>
       <div className='Card-list'>
-        <Link to={`/detail`}>
-          <Card />
-        </Link>
+        {treeList.map((tree) => (
+          <Link to={`/detail/${tree.tree_id}`} key={tree.tree_id}>
+            <Card id={tree.tree_id} />
+          </Link>
+        ))}
       </div>
       <div className='Add-tree'>
         <Link to={`/addTree`}>
