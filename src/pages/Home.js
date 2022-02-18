5008031535384fffb03769c45a1ba5ccb5aa5e44
BoomCTR <boomctr@BoomCTRs-MacBook-Pro.local>
2022-02-18 20:48:03 +0700

    update style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,25 @@
-import React from "react";
-import Card from "../components/card/Card";
-import "./Home.css";
-import { Link } from "react-router-dom";
+import React from "react"
+import Card from "../components/card/Card"
+import "./Home.css"
+import { Link } from "react-router-dom"
 export const Home = () => {
   return (
-    <div className='Home'>
-      <nav className='sticky-top navbar navbar-expand-md navbar-light bg-success'>
-        <h1 className='Home-title'>Auto Watering System </h1>
+    <div className="Home">
+      <nav className="sticky-top navbar navbar-expand-md navbar-light bg-success">
+        <h1 className="Home-title">Auto Watering System </h1>
       </nav>
-      <div className='Card-list'>
+      <div className="Card-list">
         <Link to={`/detail`}>
           <Card />
         </Link>
       </div>
-      <div className='Add-tree'>
+      <div className="Add-tree">
         <Link to={`/addTree`}>
-          <img src='./pictures/plus.png' alt='Add'></img>
+          <img src="./pictures/plus.png" alt="Add"></img>
         </Link>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
