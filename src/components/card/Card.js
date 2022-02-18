7395600dc0a47534dc96e4ca099eed39601fa171
BoomCTR <boomctr@BoomCTRs-MacBook-Pro.local>
2022-02-18 21:50:11 +0700

    test

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,20 +1,20 @@
-import "./Card.css";
-import StatusBar from "./StatusBar";
+import "./Card.css"
+import StatusBar from "./StatusBar"
 
 const Card = ({ id, name, light, humid, temp }) => {
   return (
-    <div className='Card'>
-      <div className='Card-body'>
-        <div className='Card-header'>
+    <div className="Card">
+      <div className="Card-body">
+        <div className="Card-header">
           <h3>Tree {id}</h3>
         </div>
-        <div className='Tree'>
-          <img src='./pictures/tree.png' alt='tree'></img>
+        <div className="Tree">
+          <img src="./pictures/tree.png" alt="tree"></img>
         </div>
         <StatusBar light={light} humid={humid} temp={temp} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from "react";
-import Card from "../components/card/Card";
-import "./Home.css";
-import { Link } from "react-router-dom";
-import { getAllTree } from "../services/api";
+import React, { useState, useEffect } from "react"
+import Card from "../components/card/Card"
+import "./Home.css"
+import { Link } from "react-router-dom"
+import { getAllTree } from "../services/api"
 export const Home = () => {
-  const [treeList, setTreeList] = useState([]);
+  const [treeList, setTreeList] = useState([])
 
   useEffect(() => {
     const count = setInterval(() => {
       getAllTree().then((data) => {
-        setTreeList(data.result);
-      });
+        setTreeList(data.result)
+      })
       return () => {
-        clearInterval(count);
-      };
-    }, 1000);
-  }, []);
-  console.log(treeList.result);
+        clearInterval(count)
+      }
+    }, 1000)
+  }, [])
+  console.log(treeList.result)
 
   return (
-    <div className='Home'>
-      <nav className='sticky-top navbar navbar-expand-md navbar-light bg-success'>
-        <h1 className='Home-title'>Auto Watering System </h1>
+    <div className="Home">
+      <nav className="sticky-top navbar navbar-expand-md navbar-light bg-success">
+        <h1 className="Home-title">Auto Watering System </h1>
+        <div className="leaf">
+          <img src="./leaf.png" alt=""></img>
+        </div>
       </nav>
-      <div className='Card-list'>
+      <div className="Card-list">
         {treeList.map((tree) => (
           <Link to={`/detail/${tree.tree_id}`} key={tree.tree_id}>
             <Card id={tree.tree_id} />
           </Link>
         ))}
       </div>
-      <div className='Add-tree'>
+      <div className="Add-tree">
         <Link to={`/addTree`}>
-          <img src='./pictures/plus.png' alt='Add'></img>
+          <img src="./pictures/plus.png" alt="Add"></img>
         </Link>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
