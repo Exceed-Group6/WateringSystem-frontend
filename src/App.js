4134c1c666f970eb837986acdf6ac9ac9149ae9d
abounk <a_bunk911@hotmail.com>
2022-02-18 19:03:22 +0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Addtree from "./pages/Addtree";
 import PreferencePage from "./pages/PreferencePage";
 import Detail from "./pages/Detail/Detail";
 import Home from "./pages/Home";
-// import DetailCard from "./components/DetailCard/DetailCard";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <PreferencePage />
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/addTree" element={<Addtree />}></Route>
+        <Route path="/detail/:treeId" element={<Detail />}></Route>
+        <Route path="/preference/:treeId" element={<PreferencePage />}></Route>
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
