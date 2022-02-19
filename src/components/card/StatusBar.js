b9a23fc42f8795a8f159aca1d8eb58701273fb9e
wave49192 <sirawich.tu@ku.th>
2022-02-19 14:32:50 +0700

    add status bar for home and detail page

--- src/components/card/StatusBar.js
+++ src/components/card/StatusBar.js
@@ -1,28 +1,32 @@
-import React from "react"
+import React from "react";
+import Segmentedbar from "../segmentedbar/Segmentedbar";
+import Segmentedbarhome from "../segmentedbarhome/Segmentedbarhome";
 
-import "./StatusBar.css"
+import "./StatusBar.css";
 
 export const StatusBar = ({ light, humid, temp }) => {
   return (
-    <div className="StatusBar">
-      <div className="Light">
-        <img src="./pictures/sun.png" alt=""></img>
-
-        {/* <div className="Light-bar"></div>
-        <div className="Center-dot"></div> */}
+    <div className='StatusBar'>
+      <div className='Light'>
+        <img src='./pictures/sun.png' alt=''></img>
+        <div className='Segmentedbarhome'>
+          <Segmentedbarhome percentage='32' />
+        </div>
       </div>
-      <div className="Humid">
-        <img src="./pictures/water.png" alt=""></img>
-        <div className="Humid-bar"></div>
-        {/* <div className="Center-dot"></div> */}
+      <div className='Humid'>
+        <img src='./pictures/water.png' alt=''></img>
+        <div className='Segmentedbarhome'>
+          <Segmentedbarhome percentage='62' />
+        </div>
       </div>
-      <div className="Temp">
-        <img src="./pictures/temp.png" alt=""></img>
-        <div className="Temp-bar"></div>
-        {/* <div className="Center-dot"></div> */}
+      <div className='Temp'>
+        <img src='./pictures/temp.png' alt=''></img>
+        <div className='Segmentedbarhome'>
+          <Segmentedbarhome percentage='12' />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatusBar
+export default StatusBar;
---
+++ src/components/segmentedbarhome/Segmentedbarhome.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Segmentedbarhome.css";
+const Segmentedbarhome = ({ percentage }) => {
+  const myStyle = {
+    width: "4px",
+    background: "black",
+    height: "100%",
+    position: "absolute",
+    top: "0",
+    left: `${percentage}%`,
+    opacity: "0.5",
+  };
+  return (
+    <div id='home-bar-container'>
+      <div id='home-bar'>
+        <span class='home-seg1'></span>
+        <span class='home-seg2'></span>
+        <span class='home-seg3'></span>
+        <span class='home-seg4'></span>
+        <span class='home-seg5'></span>
+      </div>
+
+      <div id='home-score' style={myStyle} />
+    </div>
+  );
+};
+
+export default Segmentedbarhome;
