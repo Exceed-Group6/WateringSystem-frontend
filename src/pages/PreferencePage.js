dfb246fac8faa5c8437e1793614cb71fd1188633
wave49192 <sirawich.tu@ku.th>
2022-02-20 00:21:16 +0700

    fasdgfdrsag

--- src/pages/PreferencePage.js
+++ src/pages/PreferencePage.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-import Switch from "../components/Switch"
-import "./PreferencePage.css"
-import { Link, useParams } from "react-router-dom"
-import { getTreeById } from "../services/api"
+import Switch from "../components/Switch";
+import "./PreferencePage.css";
+import { Link, useParams } from "react-router-dom";
+import { getTreeById } from "../services/api";
 
 function PreferencePage() {
-  const [value, setValue] = useState(false)
-  const { treeId } = useParams()
+  const [value, setValue] = useState(false);
+  const { treeId } = useParams();
 
-  const [tree, setTree] = useState({})
+  const [tree, setTree] = useState({});
 
   const onGetTreeById = (id) => {
     getTreeById(id).then((res) => {
-      setTree(res)
-    })
-  }
+      setTree(res);
+    });
+  };
 
   useEffect(() => {
-    onGetTreeById(treeId)
-    setValue(!tree.cur_bot_status)
-  }, [])
+    onGetTreeById(treeId);
+    setValue(tree.cur_bot_status);
+  }, []);
 
   const handleSubmit = () => {
-    let nametree = document.getElementById("nametree").value
-    let dis = document.getElementById("discription").value
-    let dura = document.getElementById("duration").value
-    let sunmin = document.getElementById("Sunmin").value
-    let sunmid = document.getElementById("Sunmid").value
-    let sunmax = document.getElementById("Sunmax").value
-    let watermin = document.getElementById("Watermin").value
-    let watermid = document.getElementById("Watermid").value
-    let watermax = document.getElementById("Watermax").value
-    let tempmin = document.getElementById("Tempmin").value
-    let tempmid = document.getElementById("Tempmid").value
-    let tempmax = document.getElementById("Tempmax").value
-    let bot = value
+    let nametree = document.getElementById("nametree").value;
+    let dis = document.getElementById("discription").value;
+    let dura = document.getElementById("duration").value;
+    let sunmin = document.getElementById("Sunmin").value;
+    let sunmid = document.getElementById("Sunmid").value;
+    let sunmax = document.getElementById("Sunmax").value;
+    let watermin = document.getElementById("Watermin").value;
+    let watermid = document.getElementById("Watermid").value;
+    let watermax = document.getElementById("Watermax").value;
+    let tempmin = document.getElementById("Tempmin").value;
+    let tempmid = document.getElementById("Tempmid").value;
+    let tempmax = document.getElementById("Tempmax").value;
+    let bot = value;
     // console.log(bot)
 
     let reqBody = {
@@ -47,8 +47,8 @@ function PreferencePage() {
       base_temp: [tempmin, tempmid, tempmax],
       mode_status: bot,
       duration: dura,
-    }
-    console.log(reqBody)
+    };
+    console.log(reqBody);
 
     fetch("https://ecourse.cpe.ku.ac.th/exceed06/api/updatecommand", {
       method: "PUT",
@@ -57,173 +57,173 @@ function PreferencePage() {
     })
       .then((response) => response.json())
       .then((data) => console.log(data))
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   return (
     <body>
-      <nav className="sticky-top navbar navbar-expand-md navbar-light bg-success">
-        <div className="back">
+      <nav className='sticky-top navbar navbar-expand-md navbar-light bg-success'>
+        <div className='back'>
           <Link to={`/detail/${treeId}`}>
-            <i class="fa-solid fa-angles-left fa-2x"></i>
+            <i class='fa-solid fa-angles-left fa-2x'></i>
           </Link>
         </div>
-        <div className="name">
+        <div className='name'>
           <h1>Preference : {tree.tree_name}</h1>
         </div>
-        <div className="picTree">
-          <img src="./Setting.png" alt=""></img>
+        <div className='picTree'>
+          <img src='./Setting.png' alt=''></img>
         </div>
-        <div className="save">
+        <div className='save'>
           <Link to={`/home`}>
-            <i class="fa-solid fa-floppy-disk fa-2x" onClick={handleSubmit}></i>
+            <i class='fa-solid fa-floppy-disk fa-2x' onClick={handleSubmit}></i>
           </Link>
         </div>
       </nav>
-      <div className="p-container">
-        <div className="cardBack">
-          <div className="cardbody">
-            <div className="inputBar">
-              <div className="idInput">
+      <div className='p-container'>
+        <div className='cardBack'>
+          <div className='cardbody'>
+            <div className='inputBar'>
+              <div className='idInput'>
                 {/* <i class="fa-solid fa-id-badge fa-4x"></i> */}
-                <img src="./hourglass.png" alt=""></img>
+                <img src='./hourglass.png' alt=''></img>
                 <input
                   onKeyPress={(e) => !/[0-9]/.test(e.key) && e.preventDefault()}
-                  placeholder="Duration (sec)"
-                  className="duration"
+                  placeholder='Duration (sec)'
+                  className='duration'
                   defaultValue={tree.cur_bot_duration}
-                  id="duration"
+                  id='duration'
                 ></input>
               </div>
-              <div className="threemid">
-                <div className="sunInput">
+              <div className='threemid'>
+                <div className='sunInput'>
                   {/* <i class="fa-solid fa-sun fa-4x"></i> */}
-                  <img src="./Sun.png" alt=""></img>
+                  <img src='./Sun.png' alt=''></img>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Min"
-                    className="Sunmin"
-                    pattern="[0-9]*"
-                    id="Sunmin"
+                    placeholder='Min'
+                    className='Sunmin'
+                    pattern='[0-9]*'
+                    id='Sunmin'
                     defaultValue={tree?.base_light?.set[0]}
                   ></input>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Mid"
-                    className="Sunmid"
+                    placeholder='Mid'
+                    className='Sunmid'
                     defaultValue={tree?.base_light?.set[1]}
-                    id="Sunmid"
+                    id='Sunmid'
                   ></input>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Max"
-                    className="Sunmax"
+                    placeholder='Max'
+                    className='Sunmax'
                     defaultValue={tree?.base_light?.set[2]}
-                    id="Sunmax"
+                    id='Sunmax'
                   ></input>
                 </div>
-                <div className="waterInput">
+                <div className='waterInput'>
                   {/* <i class="fa-solid fa-droplet fa-4x"></i> */}
-                  <img src="./Water.png" alt=""></img>
+                  <img src='./Water.png' alt=''></img>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Min"
-                    className="Watermin"
+                    placeholder='Min'
+                    className='Watermin'
                     defaultValue={tree?.base_humidity?.set[0]}
-                    id="Watermin"
+                    id='Watermin'
                   ></input>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Mid"
-                    className="Watermid"
+                    placeholder='Mid'
+                    className='Watermid'
                     defaultValue={tree?.base_humidity?.set[1]}
-                    id="Watermid"
+                    id='Watermid'
                   ></input>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Max"
-                    className="Watermax"
+                    placeholder='Max'
+                    className='Watermax'
                     defaultValue={tree?.base_humidity?.set[2]}
-                    id="Watermax"
+                    id='Watermax'
                   ></input>
                 </div>
-                <div className="tempInput">
+                <div className='tempInput'>
                   {/* <i class="fa-solid fa-temperature-quarter fa-4x"></i> */}
-                  <img src="./Temp.png" alt=""></img>
+                  <img src='./Temp.png' alt=''></img>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Min"
-                    className="Tempmin"
+                    placeholder='Min'
+                    className='Tempmin'
                     defaultValue={tree?.base_temp?.set[0]}
-                    id="Tempmin"
+                    id='Tempmin'
                   ></input>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Mid"
-                    className="Tempmid"
+                    placeholder='Mid'
+                    className='Tempmid'
                     defaultValue={tree?.base_temp?.set[1]}
-                    id="Tempmid"
+                    id='Tempmid'
                   ></input>
                   <input
                     onKeyPress={(e) =>
                       !/[0-9]/.test(e.key) && e.preventDefault()
                     }
-                    placeholder="Max"
-                    className="Tempmax"
+                    placeholder='Max'
+                    className='Tempmax'
                     defaultValue={tree?.base_temp?.set[2]}
-                    id="Tempmax"
+                    id='Tempmax'
                   ></input>
                 </div>
               </div>
-              <div className="botToggle">
-                <i class="fa-solid fa-robot fa-4x"></i>
-                <div className="switch">
+              <div className='botToggle'>
+                <i class='fa-solid fa-robot fa-4x'></i>
+                <div className='switch'>
                   <Switch
-                    className="switch"
+                    className='switch'
                     isOn={value}
                     handleToggle={() => setValue(!value)}
                   />
                 </div>
               </div>
             </div>
-            <div className="discri">
-              <img src="./tree.png" alt=""></img>
+            <div className='discri'>
+              <img src='./tree.png' alt=''></img>
               <br></br>
               <textarea
-                placeholder="Name Tree"
-                className="nametree"
+                placeholder='Name Tree'
+                className='nametree'
                 defaultValue={tree?.tree_name}
-                id="nametree"
+                id='nametree'
               ></textarea>
               <br></br>
               <textarea
-                placeholder="Discription..."
-                className="discription"
+                placeholder='Discription...'
+                className='discription'
                 defaultValue={tree?.tree_desc}
-                id="discription"
+                id='discription'
               ></textarea>
             </div>
           </div>
         </div>
       </div>
     </body>
-  )
+  );
 }
 
-export default PreferencePage
+export default PreferencePage;
